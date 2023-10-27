43b5c72ac95ad47cab6c1687d1a7f6579b641dc5
Georgios Rounis <georg.rounis@gmail.com>
2023-10-27 21:18:17 +0200

    Instructions steps upload

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -7,7 +7,13 @@ const ImageLinkForm = ({onInputChange, OnSubmitDetectButton}) => {
             <p className="f3">
                 {'This magic brain will detect faces in your pictures.'}
             </p>  
-            <p>{'Find an image online, paste the url and give it a shot!'}</p>
+            <p>{'To use the smart brain follow the below steps:'}</p>
+            <ol>
+                <li><i>{'1.Find online the image e.g google.'}</i></li>
+                <li><i>{'2.Press right click on the image.'}</i></li>
+                <li><i>{'3.Click on "Copy image Address".'}</i></li>
+                <li><i>{'4.Paste it and click the "Detect" button.'}</i></li>
+            </ol>
             <div className="center">
                 <div className="form center pa4 br3 shadow-5">
                     <input className="f4 pa2  w-70 center" type="tex" onChange={onInputChange}/>
