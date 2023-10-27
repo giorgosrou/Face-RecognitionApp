d4d36ff12f789f2a8eb3e0b9006850772e587cf0
Georgios Rounis <georg.rounis@gmail.com>
2023-10-27 19:38:57 +0200

    minor change

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,8 +5,9 @@ const ImageLinkForm = ({onInputChange, OnSubmitDetectButton}) => {
     return (
         <div>
             <p className="f3">
-                {'This magic brain will detect faces in your pictures. Find an image online, paste the url and give it a shot!'}
+                {'This magic brain will detect faces in your pictures.'}
             </p>  
+            <p>{'Find an image online, paste the url and give it a shot!'}</p>
             <div className="center">
                 <div className="form center pa4 br3 shadow-5">
                     <input className="f4 pa2  w-70 center" type="tex" onChange={onInputChange}/>
