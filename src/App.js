708095f53c0e7a2ef6bcacabd6055332574ae417
Georgios Rounis <georg.rounis@gmail.com>
2023-10-28 15:00:59 +0200

    minor change

--- src/App.js
+++ src/App.js
@@ -86,7 +86,8 @@ class App extends Component {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
-                id: this.state.user.id
+                id: this.state.user.id,
+                faceCount: faceCount,
             })
           })
         .then(response => response.json())
